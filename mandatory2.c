877f687e8ab01258a82c2303e146173472e691c4
Philip Mitzlaff <philipmitzlaff@Philips-MacBook-Pro.local>
2018-11-13 16:22:56 +0100

    Hinderniserkennung und Ausweichen verbessert

--- mandatory2.c
+++ mandatory2.c
@@ -23,80 +23,81 @@ int threshold_front = 100;
 int threshold_sides = 50;
 
 enum STATES {
-    NO_OBSTACLE,
-    OBSTACLE_FRONT,
-    OBSTACLE_LEFT,
-    OBSTACLE_RIGHT,
-    OBSTACLE_SIDES,
-    OBSTACLE_DEADEND
+	NO_OBSTACLE,
+	OBSTACLE_FRONT,
+	OBSTACLE_LEFT,
+	OBSTACLE_RIGHT,
+	OBSTACLE_SIDES,
+	OBSTACLE_DEADEND
 };
 void writeToDisplay(char *output) {
 	gfx_fill(0);
 	gfx_move(0, 0);
 	gfx_print_text(output);
-    return;
-}
-void turnLeft(){
-	copro_setTargetRel(-27, 27, 10);
-	delay(2000);
-	copro_stop();
 	return;
-	/*
-    copro_setSpeed(-10, 10);
-    while(1 == 1){
-    	copro_update();
-    	if(copro_distance[2]/256 < 30){
-    		copro_setSpeed(10, 10);
-    		return;
-    	}
-    	delay(500);
-    }
-    */
 }
-void turnRight(){
+void turnLeft() {
+	while (1 == 1) {
+		copro_update();
+		copro_setSpeed(-10, 15);
+		if (copro_distance[2] / 256 < 20 && copro_distance[1] / 256 < 50) {
+			copro_stop();
+			return;
+		}
+	}
+}
+void turnRight() {
+	while (1 == 1) {
+		copro_update();
+		copro_setSpeed(15, -10);
+		if (copro_distance[2] / 256 < 20 && copro_distance[3] / 256 < 50) {
+			copro_stop();
+			return;
+		}
+	}
+}
+
+void hardRight() {
+	copro_stop();
 	copro_setTargetRel(27, -27, 10);
 	delay(2000);
-	copro_stop();
 	return;
-    /*copro_setSpeed(10, -10);
-    while(1 == 1){
-    	copro_update();
-    	if(copro_distance[2]/256 < 30){
-    		copro_setSpeed(10, 10);
-    		return;
-    	}
-    	delay(500);
-    }
-    */
 }
-void checkSides(){
-    copro_update();
-    if(copro_distance[0]/256 > copro_distance[4]/256){
-        turnLeft();
-    }else{
-        turnRight();
-    }
-    return;
+void hardLeft() {
+	copro_stop();
+	copro_setTargetRel(-27, 27, 10);
+	delay(2000);
+	return;
 }
-void driveBackwards(){
-    while(1==1){
-    	copro_setSpeed(-10, -10);
-        copro_update();
-        if(copro_distance[0]/256 < 20 && copro_distance[4]/256 < 20){
-            delay(1000);
-            turnRight();
-            return;
-        }else if(copro_distance[0]/256 < 25){
-            delay(1000);
-            turnRight();
-            return;
-        }else{
-            delay(1000);
-            turnLeft();
-            return;
-        }
-        delay(1000);
-    }
+
+void checkSides() {
+	copro_update();
+	if (copro_distance[0] / 256 > copro_distance[4] / 256) {
+		turnLeft();
+	} else {
+		turnRight();
+	}
+	return;
+}
+void driveBackwards() {
+	while (1 == 1) {
+		copro_setSpeed(-10, -10);
+		copro_update();
+		if (copro_distance[0] / 256 < 20 && copro_distance[4] / 256 < 20) {
+			delay(1500);
+			hardRight();
+			return;
+		} else if (copro_distance[0] / 256 < 20) {
+			delay(1500);
+			hardRight();
+			return;
+		} else if (copro_distance[4] / 256 < 20) {
+			delay(1500);
+			hardLeft();
+			return;
+		}
+		delay(1000);
+	}
 }
 
 int main() {
@@ -107,55 +108,61 @@ int main() {
 	display_init();
 	gfx_init();
 
-    copro_ir_startMeasure();
+	copro_ir_startMeasure();
 	// Endlosschleife
 	while (1 == 1) {
-        copro_update();
-        //copro_setSpeed(10, 10);
-        if(copro_distance[2]/256 >= threshold_front){
-        	if ((copro_distance[0]/256 >= threshold_sides && copro_distance[4]/256 >= threshold_sides) || (copro_distance[1]/256 >= threshold_slopes && copro_distance[3]/256 >= threshold_slopes)){
-        		currentState = OBSTACLE_DEADEND;
-        	}else{
-        		currentState = OBSTACLE_FRONT;
-        	}
-        }else if(copro_distance[1]/256 >= threshold_slopes && copro_distance[3]/256 < threshold_slopes){
-        	currentState = OBSTACLE_RIGHT;
-        }else if(copro_distance[3]/256 >= threshold_slopes && copro_distance[1]/256 < threshold_slopes){
-            currentState = OBSTACLE_LEFT;
-        }else if(copro_distance[3]/256 >= threshold_slopes && copro_distance[1]/256 >= threshold_slopes){
-            currentState = OBSTACLE_SIDES;
-        }else{
-            currentState = NO_OBSTACLE;
-        }
+		copro_update();
+		copro_setSpeed(10, 10);
+		if (copro_distance[2] / 256 >= threshold_front) {
+			if ((copro_distance[0] / 256 >= threshold_sides
+					&& copro_distance[4] / 256 >= threshold_sides)
+					|| (copro_distance[1] / 256 >= threshold_slopes
+							&& copro_distance[3] / 256 >= threshold_slopes)) {
+				currentState = OBSTACLE_DEADEND;
+			} else {
+				currentState = OBSTACLE_FRONT;
+			}
+		} else if (copro_distance[1] / 256 >= threshold_slopes
+				&& copro_distance[3] / 256 < threshold_slopes) {
+			currentState = OBSTACLE_RIGHT;
+		} else if (copro_distance[3] / 256 >= threshold_slopes
+				&& copro_distance[1] / 256 < threshold_slopes) {
+			currentState = OBSTACLE_LEFT;
+		} else if (copro_distance[4] / 256 >= threshold_sides
+				&& copro_distance[0] / 256 >= threshold_sides) {
+			currentState = OBSTACLE_SIDES;
+		} else {
+			currentState = NO_OBSTACLE;
+		}
 
-        switch (currentState){
-            case NO_OBSTACLE:
-                writeToDisplay("NO OBSTACLE");
-                break;
-            case OBSTACLE_FRONT:
-            	copro_stop();
-                writeToDisplay("OBSTACLE: FRONT");
-                //checkSides();
-                break;
-            case OBSTACLE_LEFT:
-            	copro_stop();
-                writeToDisplay("OBSTACLE: LEFT");
-                //turnRight();
-                break;
-            case OBSTACLE_RIGHT:
-            	copro_stop();
-                writeToDisplay("OBSTACLE: RIGHT");
-                //turnLeft();
-                break;
-            case OBSTACLE_SIDES:
-                writeToDisplay("OBSTACLE: TUNNEL");
-                break;
-            case OBSTACLE_DEADEND:
-            	copro_stop();
-                writeToDisplay("OBSTACLE: DEADEND");
-                driveBackwards();
-                break;
-        }
+		switch (currentState) {
+		case NO_OBSTACLE:
+			writeToDisplay("NO OBSTACLE");
+			break;
+		case OBSTACLE_FRONT:
+			copro_stop();
+			writeToDisplay("OBSTACLE: FRONT");
+			checkSides();
+			break;
+		case OBSTACLE_LEFT:
+			//copro_stop();
+			writeToDisplay("OBSTACLE: LEFT");
+			turnRight();
+			break;
+		case OBSTACLE_RIGHT:
+			//copro_stop();
+			writeToDisplay("OBSTACLE: RIGHT");
+			turnLeft();
+			break;
+		case OBSTACLE_SIDES:
+			writeToDisplay("OBSTACLE: TUNNEL");
+			break;
+		case OBSTACLE_DEADEND:
+			copro_stop();
+			writeToDisplay("OBSTACLE: DEADEND");
+			driveBackwards();
+			break;
+		}
 		delay(1000);
 	}
 }
