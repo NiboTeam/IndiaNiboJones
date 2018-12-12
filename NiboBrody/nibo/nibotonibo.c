09ff8053bb5d4f423811f3f6a36f495a5426a5b4
Benne-P <44750548+Benne-P@users.noreply.github.com>
2018-12-12 19:47:28 +0100

    Anpassung des Nachrichtenindikators
    Der Master-NIBO erwartet bei der NIBO zu NIBO Kommunikation jetzt eine 0 als Indikator. Dadurch fasst der PC es nicht mehr auf.

--- NiboBrody/nibo/nibotonibo.c
+++ NiboBrody/nibo/nibotonibo.c
@@ -5,15 +5,24 @@ void sendMessageToNibo(int signal){
 	// signal indicates the start or end of the music
 	// this message addresses the NIBO, so the indication bit has to be 0
 	int byte = signal;
-	unsigned char message = byte & 127;
+	unsigned char message = byte & 255;
+	unsigned char answer;
 	int i = 0;
 	while(1){
 		if(!uart0_txfull()){
 			uart0_putchar(message);
 		}
 		delay(200);
-		if (!uart0_rxempty() || i == 25){
+		if (!uart0_rxempty()){
 			// wait for answer
+			answer = uart0_getchar();
+			byte = answer;
+			if((byte & 128) != 0){
+				// wrong indicator
+				continue;
+			}
+			break;
+		}else if (i == 25){
 			break;
 		}
 		i++;
